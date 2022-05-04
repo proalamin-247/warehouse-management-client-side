26b20e2dac1132abcc8594894fad2bb535c90cbf
proalamin-247 <pro.alamin247@gmail.com>
2022-05-04 12:34:33 +0600

    loading spinner added

---
+++ src/Pages/Shared/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const Loading = () => {
+    return (
+        <div style={{ height: '400px' }} className='w-100 d-flex justify-content-center align-items-center'>
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="success" />
+        </div>
+    );
+};
+
+export default Loading;

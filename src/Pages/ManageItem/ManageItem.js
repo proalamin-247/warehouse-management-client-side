ffdcd1c6d81d17012ffeb546c56d94e6bb5b6a93
proalamin-247 <pro.alamin247@gmail.com>
2022-05-04 10:38:09 +0600

    all pages setup done and react router install

---
+++ src/Pages/ManageItem/ManageItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ManageItem.css';
+
+const ManageItem = () => {
+    return (
+        <div>
+            <h1>This is ManageItem area</h1>
+        </div>
+    );
+};
+
+export default ManageItem;
---
+++ src/Pages/MyItem/MyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyItem.css';
+
+const MyItem = () => {
+    return (
+        <div>
+            <h1>This is my item area</h1>
+        </div>
+    );
+};
+
+export default MyItem;

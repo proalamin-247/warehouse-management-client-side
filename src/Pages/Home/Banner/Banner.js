5e4d050701f11bc803b05a4e424d7b3b30d5612c
proalamin-247 <pro.alamin247@gmail.com>
2022-05-04 11:07:03 +0600

    home page banner section added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../../images/banner.jpg'
+
+const Banner = () => {
+    return (
+        <div className='container mt-4 '>
+            {/* <Carousel activeIndex={index} onSelect={handleSelect}> */}
+            <Carousel>
+                <Carousel.Item className='rounded'>
+                    <img
+                        className="d-block w-100 rounded"
+                        src={banner1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3 className='text-primary bg-white'>Warehouse Management</h3>
+                        <p className='bg-primary'>Check your products quantity and restocks</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
